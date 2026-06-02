a11efd4edf36c04cd5244c10845530324b16c475
treachxry <CreatureOfSilliness@proton.me>
2026-06-02 19:53:11 +0200

    use hash routing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    base: '/ao3-theme-generator',
+    base: '/ao3-theme-generator/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
